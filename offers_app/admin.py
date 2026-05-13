9e2345967d266e83ca103f277434cd873fe0da74
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-05-13 19:42:20 +0200

    fix: media path added

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from offers_app.models import Offer
+
+
 # Register your models here.
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'user', 'has_image', 'created_at', 'updated_at']
+
+    def has_image(self, obj):
+        return  bool(obj.image)
+
+    has_image.boolean = True
+
+
+admin.site.register(Offer, OfferAdmin)
