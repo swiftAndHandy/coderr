6ee5280bafdf6a4d309fb4e7e872a9a61f10356f
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-05-13 07:56:30 +0200

    feat: order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    customer_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer_orders')
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='business_orders')
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField()
+    offer_type = models.CharField(max_length=100, choices=[
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium'),
+    ], default='basic')
+    status = models.CharField(max_length=12, choices=[
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ], default='in_progress')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
