9fda809f58a149ad83f981c040186679c480ece2
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-05-13 14:10:03 +0200

    fix: orders_app urls: fixed int param

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -5,5 +5,5 @@ from orders_app.api.views import OrderListCreateAPIView, OrderUpdateDestroyView,
 urlpatterns = [
     path('orders/', OrderListCreateAPIView.as_view()),
     path('orders/<int:pk>/', OrderUpdateDestroyView.as_view()),
-    path('order-count/<int:pk>/', OrdersInProgressCountView.as_view()),
+    path('order-count/<int:business_user_id>/', OrdersInProgressCountView.as_view()),
 ]
